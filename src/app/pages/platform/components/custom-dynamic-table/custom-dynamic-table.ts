c96d01369528ca0efc7b85011521c4afaec234fe
Ruben <rubenf85@gmail.com>
2026-05-28 00:25:37 +0200

    CustomDynamicTable component added

---
+++ src/app/pages/platform/components/custom-dynamic-table/custom-dynamic-table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-dynamic-table',
+  imports: [],
+  templateUrl: './custom-dynamic-table.html',
+  styleUrl: './custom-dynamic-table.scss',
+})
+export class CustomDynamicTable {}
