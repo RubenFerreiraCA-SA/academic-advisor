279a63fb532dcacdddca17eef9dd4f0da92b5530
Ruben <rubenf85@gmail.com>
2026-05-27 23:32:47 +0200

    Update to platform side nav

--- src/app/pages/platform/platform.ts
+++ src/app/pages/platform/platform.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-platform',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './platform.html',
   styleUrl: './platform.scss',
 })
