85846e50b56a55b16ba8bde9bc6da642487ef710
Ruben <rubenf85@gmail.com>
2026-05-28 00:14:16 +0200

    Top Nav extracted to own component

--- src/app/pages/platform/platform.ts
+++ src/app/pages/platform/platform.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNav } from './sections/side-nav/side-nav';
+import { TopNav } from './sections/top-nav/top-nav';
 
 @Component({
   selector: 'app-platform',
-  imports: [RouterOutlet, SideNav],
+  imports: [RouterOutlet, SideNav, TopNav],
   templateUrl: './platform.html',
   styleUrl: './platform.scss',
 })
