4d0575bc2a512eec193c196da20bbbe0506a643e
Ruben <rubenf85@gmail.com>
2026-05-27 21:25:57 +0200

    Set up of basic commercial site

---
+++ src/app/commercial/pages/landing-page/landing-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../shared/navbar/navbar';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [Navbar],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.scss',
+})
+export class LandingPage {}
