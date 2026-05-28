5aa9c515be0b1cda717d158ac42ed7974518eb13
Ruben <rubenf85@gmail.com>
2026-05-29 01:00:58 +0200

    Improvements made to top nav

--- src/app/pages/platform/sections/top-nav/top-nav.ts
+++ src/app/pages/platform/sections/top-nav/top-nav.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
 
+export interface TopNavConfig {
+  notifications: number;
+  mails: number;
+  client: {
+    title: string;
+    firstName: string;
+    lastName: string;
+    avatar?: string;
+    jobTitle: string;
+  }
+}
+
 @Component({
   selector: 'app-top-nav',
   imports: [],
   templateUrl: './top-nav.html',
   styleUrl: './top-nav.scss',
 })
-export class TopNav {}
+export class TopNav {
+  topNavConfig: TopNavConfig = {
+    notifications: 5,
+    mails: 3,
+    client: {
+      title: 'Dr.',
+      firstName: 'Alex',
+      lastName: ' Morgan',
+      jobTitle: 'Software Engineer'
+    }
+  };
+
+}
